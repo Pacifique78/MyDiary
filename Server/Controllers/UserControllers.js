0fe7e994a219959356cbc40ad1f1daae37f50a75
Pacifique78 <tuyipaccy78@gmail.com>
2019-10-06 21:39:18 +0200

    add and test signup endpoint [Finishes #168967511]

---
+++ Server/Controllers/UserControllers.js
@@ -0,0 +1,39 @@
+import bcrypt from 'bcrypt';
+import users from '../Model/userModel';
+import jwt from 'jsonwebtoken';
+class usersClass{
+    async createUser(req, res){
+        const{email} = req.body;
+        const userFound = users.find(user => user.email === email);
+        if(userFound){
+            res.status(409).json({
+                status: 409,
+                'error': 'user with such email already exists'
+            });
+        }
+        else{
+            const {firstName, lastName, email, password} = req.body;
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            const newUser = {firstName, lastName, email, hashedPassword};
+            users.push(newUser);
+            const token = await jwt.sign({
+                firstName, lastName, email
+            }, process.env.secret, {
+                expiresIn: '7d'
+            });
+            return res.status(201).json({
+                status:201,
+                message: 'User created',
+                data: {
+                    firstName,
+                    lastName,
+                    email
+                },
+                token
+            });
+        }
+    }
+}
+const newClass = new usersClass();
+export default newClass;
