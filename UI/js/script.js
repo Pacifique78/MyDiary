70014f394d76941cb678ec82863cd9930c6d839e
Pacifique78 <tuyipaccy78@gmail.com>
2019-10-16 06:49:38 +0200

    deploy heroku

---
+++ UI/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('login-btn').addEventListener('click', () => {
+    document.getElementById('login').style.display = 'flex';
+    document.getElementById('login-form').style.display = 'inline';
+});
