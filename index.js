d736845ab8ab33a80cc44978257a96f9d7b7e7ce
Pacifique78 <tuyipaccy78@gmail.com>
2019-11-01 18:12:29 +0200

    ch(swagger): Add swagger for API documantation {Finishes #16298432]

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import swaggerUiexpress from 'swagger-ui-express';
 import userRoutes from './Server/Routes/userRoutes';
 import entryRoutes from './Server/Routes/entryRoutes';
+import swagger from './swagger';
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => res.status(200).json({ message: 'WELCOME TO MY DIARY' }));
 app.use(userRoutes);
 app.use(entryRoutes);
-const port = process.env.PORT || 6000;
+app.use('/api/swagger/', swaggerUiexpress.serve, swaggerUiexpress.setup(swagger));
+const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening to port ${port}`));
 export default app;
