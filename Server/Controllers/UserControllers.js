30679deed4b7e39fdf168d491796130c77d44aa4
Pacifique78 <tuyipaccy78@gmail.com>
2019-11-03 22:11:27 +0200

    feat(get specific entry): add logic for get specific entry endpoint and implement tests for all possible requests [Finishes #169523877]

--- Server/Controllers/UserControllers.js
+++ Server/Controllers/UserControllers.js
@@ -7,78 +7,58 @@ import checkPassword from '../helpers/checkPassword';
 dotenv.config();
 class UsersClass {
     async createUser(req, res) {
-        try {
-            const {
-                firstName, lastName, email, password,
-            } = req.body;
-            const selectQuery = 'SELECT * FROM users where email=$1 ;';
-            const value = [email];
-            const rows = await querry(selectQuery, value);
-            if (rows[0]) {
-                return res.status(409).json({
-                    status: 409,
-                    error: `user with ${email} already exists`,
-                });
-            }
-            const pass = hashPassword(password);
-            const insertQuery = `INSERT INTO users (firstname, lastname, email, password) 
-            VALUES ($1, $2, $3, $4) RETURNING *;`;
-            const values = [firstName, lastName, email, pass];
-            const result = await querry(insertQuery, values);
-            const { id } = result[0];
-            const token = generateToken(id, email);
-            return res.status(201).json({
-                status: 201,
-                message: 'User created',
-                data: {
-                    id,
-                    firstName,
-                    lastName,
-                    email,
-                },
-                token,
-            });
-        } catch (error) {
-            const message = error.message || 'Unknown error occured';
-            res.status(500).json({
-                status: 500,
-                error: {
-                    message,
-                },
+        const {
+            firstName, lastName, email, password,
+        } = req.body;
+        const selectQuery = 'SELECT * FROM users where email=$1 ;';
+        const value = [email];
+        const rows = await querry(selectQuery, value);
+        if (rows[0]) {
+            return res.status(409).json({
+                status: 409,
+                error: `user with ${email} already exists`,
             });
         }
+        const pass = hashPassword(password);
+        const insertQuery = `INSERT INTO users (firstname, lastname, email, password) 
+            VALUES ($1, $2, $3, $4) RETURNING *;`;
+        const values = [firstName, lastName, email, pass];
+        const result = await querry(insertQuery, values);
+        const { id } = result[0];
+        const token = generateToken(id, email);
+        return res.status(201).json({
+            status: 201,
+            message: 'User created',
+            data: {
+                id,
+                firstName,
+                lastName,
+                email,
+            },
+            token,
+        });
     }
 
     async login(req, res) {
-        try {
-            const { email, password } = req.body;
-            const selectQuery = 'SELECT * FROM users where email=$1 ;';
-            const value = [email];
-            const rows = await querry(selectQuery, value);
-            if (rows[0] && checkPassword(password, rows[0].password)) {
-                const { id } = rows[0];
-                const token = generateToken(id, email);
-                return res.status(200).json({
-                    status: 200,
-                    message: 'User logged in successfully',
-                    data: {
-                        token,
-                    },
-                });
-            }
-            return res.status(401).json({
-                status: 401,
-                error: 'Invalid username / password',
-            });
-        } catch (error) {
-            const message = error.message || 'Unknown error occured';
-            res.status(500).json({
-                status: 500,
-                error: {
-                    message,
+        const { email, password } = req.body;
+        const selectQuery = 'SELECT * FROM users where email=$1 ;';
+        const value = [email];
+        const rows = await querry(selectQuery, value);
+        if (rows[0] && checkPassword(password, rows[0].password)) {
+            const { id } = rows[0];
+            const token = generateToken(id, email);
+            return res.status(200).json({
+                status: 200,
+                message: 'User logged in successfully',
+                data: {
+                    token,
                 },
             });
         }
+        return res.status(401).json({
+            status: 401,
+            error: 'Invalid username / password',
+        });
     }
 }
 const newClass = new UsersClass();
