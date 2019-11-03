5e404d84b413f0938c500d95bc75e64732f5c523
Pacifique78 <tuyipaccy78@gmail.com>
2019-11-03 16:13:40 +0200

    feat(sign in): add and test sign in API endpoint
    -check the if user has an account
    -create a token
    
    [Finishes #169522520]

--- Server/Test/userTest.js
+++ Server/Test/userTest.js
@@ -66,3 +66,36 @@ describe('User SignUp', () => {
             });
     });
 });
+describe('User Signin', () => {
+    it('Should allow a user to signin', (done) => {
+        chai.request(app).post('/api/v2/auth/signin')
+            .send(testUser[2])
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.property('message');
+                expect(res.body).to.have.property('data');
+                expect(res.body.data).to.have.property('token');
+                done();
+            });
+    });
+    it('Should NOT allow a user to signin: Email not found or incorrect password', (done) => {
+        chai.request(app).post('/api/v2/auth/signin')
+            .send(testUser[3])
+            .end((err, res) => {
+                expect(res).to.have.status(401);
+                expect(res.body).to.have.property('error');
+                expect(res.body.error).to.equal('Invalid username / password');
+                done();
+            });
+    });
+    it('Should NOT allow a user to signin: Invalid input or missing input', (done) => {
+        chai.request(app).post('/api/v2/auth/signin')
+            .send(testUser[5])
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body).to.have.property('error');
+                expect(res.body.error).to.equal(' password  is required');
+                done();
+            });
+    });
+});
